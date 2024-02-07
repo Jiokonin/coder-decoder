0f9f19d816c6356bcdb10221a69cd41a8dabeb68
Jiokonin <157647208+Jiokonin@users.noreply.github.com>
2024-02-07 04:02:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let textOutput = document.querySelector("#output");
 function criptografar(){
     let texto = textInput.value;
 
-    let lockCripto = texto.replace(/a/g, "jules").replace(/e/g, "retro").replace(/i/g, "warp")
-    .replace(/o/g, "tartus").replace(/u/g, "thou");
+    let lockCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
+    .replace(/o/g, "ober").replace(/u/g, "ufat");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="texto">' + lockCripto + 
     '</textarea>' + '<button class="btn__copiar" id="copiar" onclick="copiar()">copiar<button>';
@@ -15,8 +15,8 @@ function criptografar(){
 function descriptografar(){
     let texto = textInput.value;
 
-    let unlockCripto = texto.replace(/jules/g, "a").replace(/retro/g, "e").replace(/warp/g, "i")
-    .replace(/tartus/g, "o").replace(/thou/g, "u");
+    let unlockCripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a")
+    .replace(/ober/g, "o").replace(/ufat/g, "u");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="texto">' + unlockCripto + 
     '</textarea>' + '<button class="btn__copiar" id="copiar" onclick="copiar()">copiar<button>';
